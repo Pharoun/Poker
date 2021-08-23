eb66d5d797e09525d55b8bf419fcb20d7f531f2f
Pharoun <paulharoun@live.com>
2021-08-24 00:29:02 +0300

    progress with player spawn positions

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -8,38 +8,118 @@ public class player : NetworkBehaviour
 {
 
     public Slider slider;
+    public GameObject buttons;
     public Text betText;
     public Image chips;
     public Image pic;
+    public Text name1;
 
-    public Image chips2;
-    public Image pic2;
+    //public Image pic2;
+    //public Text name2;
+
+    //public Image pic3;
+    //public Text name3;
+    //public Image pic4;
+    //public Text name4;
 
     public Sprite[] chipsImages;
     public Sprite[] avatars;
+
     private int betTotal;
+    //private int count;
+
+    string randomString = string.Empty;
+
+    GameObject[] players;
 
     void Start()
     {
 
-        chips2 = GameObject.FindGameObjectWithTag("chips2").GetComponent<Image>();
-        pic2 = GameObject.FindGameObjectWithTag("pic2").GetComponent<Image>();
+        //pic2 = GameObject.FindGameObjectWithTag("pic2").GetComponent<Image>();
+        //name2 = GameObject.FindGameObjectWithTag("name2").GetComponent<Text>();
+        pic.sprite = avatars[Random.Range(0, 6)];
+        CreateRandomString(7);
+        name1.text = randomString;
 
-        if (isLocalPlayer)
+        //count = 0;
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        //foreach (GameObject go in players)
+        //{
+        //    count++;
+        //}
+
+        CmdShowPlayer();
+    }
+
+    [Command(requiresAuthority = false)]
+    void CmdShowPlayer()
+    {
+        RpcShowPlayer();
+    }
+
+    [ClientRpc]
+    void RpcShowPlayer()
+    {
+
+        if (hasAuthority)
         {
 
-            gameObject.transform.parent = GameObject.FindGameObjectWithTag("players").transform;
-            gameObject.transform.localPosition = new Vector3(0, -210, 0);
-            gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            gameObject.transform.parent = GameObject.FindGameObjectWithTag("player1").transform;
+            gameObject.transform.localPosition = new Vector3(-70, 0, 0);
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
 
-            chips.sprite = chipsImages[0];
-            pic.sprite = avatars[0];
+            //foreach (GameObject go in players)
+            //{
+            //    if (go != gameObject)
+            //    {
+            //        foreach (Transform child in go.transform)
+            //        {
+            //            if (child.tag == "pic")
+            //                pic2.sprite = child.GetComponent<Image>().sprite;
+
+            //            if (child.tag == "name")
+            //                name2.text = child.GetComponent<Text>().text;
+            //        }
+            //    }
+            //}
+
         }
         else
         {
-            chips2.sprite = chipsImages[0];
-            pic2.sprite = avatars[1];
-            // control player auto creation
+            if(players.Length == 2)
+            {
+                gameObject.transform.parent = GameObject.FindGameObjectWithTag("player2").transform;
+                gameObject.transform.localPosition = new Vector3(0, 0, 0);
+                gameObject.transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            if (players.Length == 3)
+            {
+                gameObject.transform.parent = GameObject.FindGameObjectWithTag("player3").transform;
+                gameObject.transform.localPosition = new Vector3(0, 0, 0);
+                gameObject.transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            if (players.Length == 4)
+            {
+                gameObject.transform.parent = GameObject.FindGameObjectWithTag("player4").transform;
+                gameObject.transform.localPosition = new Vector3(0, 0, 0);
+                gameObject.transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            slider.gameObject.SetActive(false);
+            buttons.SetActive(false);
+        }
+    }
+
+    private void CreateRandomString(int stringLength = 7)
+    {
+        int _stringLength = stringLength - 1;
+        string[] characters = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        for (int i = 0; i <= _stringLength; i++)
+        {
+            randomString = randomString + characters[Random.Range(0, characters.Length)];
         }
     }
 
